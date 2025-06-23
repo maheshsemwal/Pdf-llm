f0469ea232e491b9e4d3d4ce2fd7d67ff306cf45
maheshsemwal <maheshsemwal04@gmail.com>
2025-06-24 02:40:21 +0530

    added port

--- backend/main.py
+++ backend/main.py
@@ -27,3 +27,9 @@ app.include_router(pdf_router)
 app.include_router(process.router)
 app.include_router(question_router)
 app.include_router(chat_router)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
