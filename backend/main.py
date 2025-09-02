cbd53dd097d049c912889f122206073275d5b1db
Mahesh Semwal <105265591+maheshsemwal@users.noreply.github.com>
2025-09-02 23:41:08 +0530

    Use environment variable for server port

--- backend/main.py
+++ backend/main.py
@@ -38,4 +38,6 @@ app.include_router(chat_router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0",port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
